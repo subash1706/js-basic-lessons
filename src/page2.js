612f0889b59a82fe781cef481d19634fb853c43b
subash1706 <subash.somasundaram@chainsys.com>
2022-03-02 14:00:05 +0530

    1stmar commit

---
+++ src/page2.js
@@ -0,0 +1,13 @@
+/* eslint-disable space-before-function-paren */
+function addNumbers(a, b) {
+  // eslint-disable-next-line semi
+  return a + b;
+}
+// eslint-disable-next-line semi
+console.log(`2 + 3 => ${addNumbers(2, 3)}`);
+function multiplyNumbers(a, b) {
+  // eslint-disable-next-line semi
+  return a * b;
+}
+// eslint-disable-next-line semi
+console.log(`2 * 3 => ${multiplyNumbers(2, 3)}`);
---
+++ src/samplec.js
@@ -0,0 +1,19 @@
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+const q = document.querySelectconstor("p");
+const p = document.querySelector("p");
+
+// town = "chennai"
+// console.log(town);
+
+/*
+ *  Using a variable without declaring the variable
+ */
+
+const city = "Chennai";
+console.log(city);
+const age = 100;
+// console.log(q);
+// console.log(p);
+// console.log(age);
